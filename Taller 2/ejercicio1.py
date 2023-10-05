2f4845ca31acedc8967e0a0fc5c9b8e577e9f8b2
AlejandroP75 <69017469+AlejandroP75@users.noreply.github.com>
2023-10-05 12:02:12 -0500

    01 Commit

--- Taller 2/ejercicio1.py
+++ Taller 2/ejercicio1.py
@@ -1,46 +1,86 @@
+import math
+
 while True:
     fechaNacimiento = input("Ingrese la fecha de su nacimiento en el siguiente formato (dia/mes/año)\n"
                         "--> ")
     fNlist = fechaNacimiento.split("/")
-    if (int(fNlist[2]) < 1) or (int(fNlist[2]) > 2023):
+    diaN = int(fNlist[0])
+    mesN = int(fNlist[1])
+    añoN = int(fNlist[2])
+    if (añoN < 1) or (añoN > 2023):
         print("El año tiene que estar entre 1 y 2023, vuelve a intentarlo")
         continue
-    elif (int(fNlist[1]) < 1) or (int(fNlist[1]) > 12):
+    elif (mesN < 1) or (mesN > 12):
         print("El mes tiene que estar entre 1 y 12, vuelve a intentarlo")
         continue
-    
     bisiesto = False
-    if ((int(fNlist[2]) % 4) == 0):
+    if ((añoN % 4) == 0):
         bisiesto = True
-        if((int(fNlist[2]) % 100) == 0):
-            if((int(fNlist[2]) % 400) != 0):
+        if((añoN % 100) == 0):
+            if((añoN % 400) != 0):
                 bisiesto = False
-    if (int(fNlist[1]) == 1) or (int(fNlist[1]) == 3) or (int(fNlist[1]) == 5) or (int(fNlist[1]) == 7) or (int(fNlist[1]) == 8) or (int(fNlist[1]) == 10) or (int(fNlist[1]) == 12):
-        if(int(fNlist[0]) < 0) or (int(fNlist[0]) > 31):
+    if (mesN == 1) or (mesN == 3) or (mesN == 5) or (mesN == 7) or (mesN == 8) or (mesN == 10) or (mesN == 12):
+        if(diaN < 0) or (diaN > 31):
             print("El numero de días es mayor al posible de ese mes, vuelve a intentarlo")
             continue
-    if (int(fNlist[1]) == 4) or (int(fNlist[1]) == 6) or (int(fNlist[1]) == 9) or (int(fNlist[1]) == 11):
-        if(int(fNlist[0]) < 0) or (int(fNlist[0]) > 30):
+    if (mesN == 4) or (mesN == 6) or (mesN == 9) or (mesN == 11):
+        if(diaN < 0) or (diaN > 30):
             print("El numero de días es mayor al posible de ese mes, vuelve a intentarlo")
             continue
-    if (int(fNlist[1]) == 2):
+    if (mesN == 2):
         if(bisiesto == True):
-            if(int(fNlist[0]) < 0) or (int(fNlist[0]) > 29):
+            if(diaN < 0) or (diaN > 29):
                 print("El numero de días es mayor al posible de ese mes, vuelve a intentarlo")
                 continue    
         else:
-            if(int(fNlist[0]) < 0) or (int(fNlist[0]) > 28):
+            if(diaN < 0) or (diaN > 28):
                 print("El numero de días es mayor al posible de ese mes, vuelve a intentarlo")
-                continue  
-
-
+                continue
+    break  
 
 while True:
     fechaActual = input("Ingrese la fecha actual en el siguiente formato (dia/mes/año)\n"
                         "-->")
     fAlist = fechaActual.split("/")
+    diaA = int(fAlist[0])
+    mesA = int(fAlist[1])
+    añoA = int(fAlist[2])
+    if (añoN < 1) or (añoN > 2023):
+        print("El año tiene que estar entre 1 y 2023, vuelve a intentarlo")
+        continue
+    elif (mesN < 1) or (mesN > 12):
+        print("El mes tiene que estar entre 1 y 12, vuelve a intentarlo")
+        continue
+    bisiesto = False
+    if ((añoN % 4) == 0):
+        bisiesto = True
+        if((añoN % 100) == 0):
+            if((añoN % 400) != 0):
+                bisiesto = False
+    if (mesN == 1) or (mesN == 3) or (mesN == 5) or (mesN == 7) or (mesN == 8) or (mesN == 10) or (mesN == 12):
+        if(diaN < 0) or (diaN > 31):
+            print("El numero de días es mayor al posible de ese mes, vuelve a intentarlo")
+            continue
+    if (mesN == 4) or (mesN == 6) or (mesN == 9) or (mesN == 11):
+        if(diaN < 0) or (diaN > 30):
+            print("El numero de días es mayor al posible de ese mes, vuelve a intentarlo")
+            continue
+    if (mesN == 2):
+        if(bisiesto == True):
+            if(diaN < 0) or (diaN > 29):
+                print("El numero de días es mayor al posible de ese mes, vuelve a intentarlo")
+                continue    
+        else:
+            if(diaN < 0) or (diaN > 28):
+                print("El numero de días es mayor al posible de ese mes, vuelve a intentarlo")
+                continue
+    totalN = diaN + (mesN * 30) + (añoN * 365)
+    totalA = diaA + (mesA * 30) + (añoA * 365)
+    if (totalA < totalN):
+        print("La fecha de nacimiento no puede ser mayor a la fecha actual, vuelve a intentarlo")
+        continue
+    break
 
-
-
-print(fechaActual)
-print(fechaNacimiento)
+edad = math.floor((totalA-totalN)/365)
+print(f"Usted tiene : {edad} años"
+      "Gracias por usar nuestro programa")
